544745c7183a3f788b94e15525466d6d47d3ee83
Nícolas Gabriel Bubans <146142653+buabns@users.noreply.github.com>
2026-08-20 11:48:28 -0300

    Add parity check for user input number

--- aula-04/exercicios.py
+++ aula-04/exercicios.py
@@ -1,1 +1,9 @@
+numero = int (input("Digite um número: "))
+resto = numero % 3
+
+
+if resto == 1:
+  print("Seu número é: {}, ele é impar".format(numero))
+else:
+  print("Seu número é: {}, ele é par".format(numero))
 
