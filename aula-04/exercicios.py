b432d50a3106ef5e6e39925a234cb0bd295fe5ee
Nícolas Gabriel Bubans <146142653+buabns@users.noreply.github.com>
2026-08-20 11:48:38 -0300

    Delete aula-04/exercicios.py

--- aula-04/exercicios.py
+++
@@ -1,9 +0,0 @@
-numero = int (input("Digite um número: "))
-resto = numero % 3
-
-
-if resto == 1:
-  print("Seu número é: {}, ele é impar".format(numero))
-else:
-  print("Seu número é: {}, ele é par".format(numero))
-
